459c2dd425b3cf748afc9a8f778540113ab659b6
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-07-19 12:38:33 -0400

    Add reviews class

--- coffee_and_books/urls.py
+++ coffee_and_books/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from reviews import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', views.home, name='home')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=75)
+    isbn = models.CharField(max_length=25)
+    genre = models.CharField(max_length=25, default='Genre')
+    review = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='reviews/images/')
+    url = models.URLField(blank=True)
